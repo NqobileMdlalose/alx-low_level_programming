d64ce96ce540eeac8ea92daceca4e077be66fab3
NqobileMdlalose <mdlalose150@gmail.com>
2023-03-16 06:51:47 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-/*prints a srting of character*/
-int main()
+/**
+ * prints a srting of character
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
