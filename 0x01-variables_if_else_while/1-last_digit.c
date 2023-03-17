6659b430fab3873682cab29f2087619bad633a26
NqobileMdlalose <mdlalose@gmail.com>
2023-03-17 15:02:30 +0000

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 	}
 	if (Last_digit < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, Last_digit);
+		unsigned printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Last_digit);
 	}
 	return (0);
 }
