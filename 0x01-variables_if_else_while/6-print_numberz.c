d5ce8d9aedc30b3e12691e762bfa3e1eb9648cb1
NqobileMdlalose <mdlalose@gmail.com>
2023-03-17 20:53:21 +0000

    numbuz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints out single numbers of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		i = putchar(i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
