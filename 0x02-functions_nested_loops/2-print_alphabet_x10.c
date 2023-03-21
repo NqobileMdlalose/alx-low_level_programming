c7f170f059e67f2cf6e61e99cdc38022fb99c623
NqobileMdlalose <mdlalose150@gmail.com>
2023-03-21 02:50:01 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,19 @@
 
 /**
  * print_alphabet_x10 - Prints alpahbet 10 time
- * 
+ *
  * Return: (void)
  */
 void print_alphabet_x10(void)
 {
 	int i;
 	char x;
-	
+
 	i = 0;
 	while (i < 10)
 	{
-		x = 'a';
-		while (x < 'x')
+		x = 97;
+		while (x <= 122)
 		{
 		_putchar(x);
 		x++;
