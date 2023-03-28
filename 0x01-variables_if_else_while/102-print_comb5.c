ae81370fd9d1dc57ec8fbffe77097abcf9bbbfac
NqobileMdlalose <mdlalose@gmail.com>
2023-03-28 14:54:23 +0000

    updating git

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints a combination os 2 2 digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, n;
+
+	for ()
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			putchar(i);
+		
+			for(j = 48; j <= 57; j++)
+			{
+				putchar(j);
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
+}
