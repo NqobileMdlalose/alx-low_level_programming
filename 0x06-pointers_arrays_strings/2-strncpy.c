98c1fad5d2a96c23155791c5beb7539072453da4
NqobileMdlalose <mdlalose150@gmail.com>
2023-03-29 11:15:37 -0700

    coping a complicated string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
