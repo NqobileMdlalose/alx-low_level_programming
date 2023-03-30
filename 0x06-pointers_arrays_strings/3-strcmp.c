e7638ac041c49cd4e591cb21d2b48d7c3106ebd5
NqobileMdlalose <mdlalose150@gmail.com>
2023-03-30 01:42:31 -0700

    comparing 2 strins

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,21 +12,14 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 	j = 0;
-	neg = i - j;
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	if (i > j || i != j)
+	neg = s1[i] - s2[j];
+	if (s1[i] > s2[j] || s1[i] != s2[j])
 	{
 		return (neg);
 	}
-	else if (i < j || i != j)
+	else if (s1[i] < s2[j] || s2[i] != s2[j])
 	{
+
 		return (neg);
 	}
 	else
