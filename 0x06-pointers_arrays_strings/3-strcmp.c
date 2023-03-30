9bc96cca511300df39c20026b03a0117f9235820
NqobileMdlalose <mdlalose150@gmail.com>
2023-03-30 03:19:31 -0700

    compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,21 +8,21 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, neg;
+	int i = 0;
+	int neg;
 
-	neg = s1[i] - s2[j];
-	while (s1[i] != '\0' && s2[j] != '\0')
+	neg = s1[i] - s2[i];
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] > s2[j])
+		if (s1[i] > s2[i])
 		{
 			return (neg);
 		}
-		else if (s1[i] < s2[j])
+		else if (s1[i] < s2[i])
 		{
 			return (neg);
 		}
 		i++;
-		j++;
 	}
 	return (0);
 }
