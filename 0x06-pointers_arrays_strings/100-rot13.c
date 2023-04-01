ee0893f2b9867dd90403faf059bdfa4d72f964b2
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-01 12:00:11 -0700

    need to make some fixes code has bugs

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @a: pointer
+ * Return: char
+ */
+char *rot13(char *a)
+{
+	char *alpha1 = "abcdefghijklm";
+	char *alpha2 = "nopqrstuvwxyz";
+	int i;
+	
+	while (alpha1[i] != '\0')
+	{
+		while (a[i] <= 13)
+		{
+			alpha1[i] = alpha2[i];
+			i++;
+		}
+		i++;
+	}
+	return (a);
+}
