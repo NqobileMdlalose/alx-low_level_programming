3e52496a8ab41d7067101ec39881428132292581
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-03 08:26:10 -0700

    code needs fixing

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+ 
+/**
+ * _strspn - gets the length of a prefix substing
+ * @s: string 1
+ * @accept: string 2
+ * Return: int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int count = 0;
+
+	while (s[i] && accept[i])
+	{
+		if (s[i] != accept[i])
+		{
+			break;
+		}		
+		i++;
+	}
+	count++;
+	return (count);
+}
