3a90cb364a2294a33a0adfda21e2f00bbf01068c
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-03 04:57:05 -0700

    memory set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	char *s1 = s;
 
-	while (i != n)
+	while (n--)
 	{
-		*s++ = b;
-		i++;
+		*s1++ = b;
 	}
 	return (s);
 }
