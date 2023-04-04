c30a624f8cbf4d5c63244e3cfcacf49f9ff86bb6
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-04 02:00:32 -0700

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,14 +9,15 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
+	char *p = needle;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		for (j = 0; p[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
+			if (haystack[i] == p[j])
 			{
-				return (needle);
+				return (p);
 			}
 		}
 	}
