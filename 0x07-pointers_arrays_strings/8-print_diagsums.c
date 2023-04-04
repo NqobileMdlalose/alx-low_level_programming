3317f914460bbb1b32fcca796357dd0f8741e9b0
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-04 11:50:29 -0700

    prints sum of diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,16 +9,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2;
+	int sum1 = 0, sum2 = 0;
 	int i;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + (i * size) + 1);
-	}
-	for (i = 0; i < size; i++)
-	{
-		sum2 += *(a + (i * size) + (2 - i));
+		sum1 += *(a + i * size + 1);
+		sum2 += *(a + i * size + size - i - 1);
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
