685aa849b62974534e6f68cb3340c563048da414
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-05 13:53:50 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * sqrt_func - recursion function
+ * @n: integer
+ * @i: integer
+ * Return: int
+ */
+int sqrt_func(int n, int bot, int top)
+{
+	int mid;
+
+	mid = (top + bot) / 2;
+
+	if (mid == bot)
+	{
+		return (mid);
+	}
+	if (mid * mid < n)
+	{	
+		return (sqrt_func(n, mid, top));
+	}
+	return (sqrt_func(n, bot, mid));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ * Return: integer
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (sqrt_func(n, 0, n + 1));
+}
