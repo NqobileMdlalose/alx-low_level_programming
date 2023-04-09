900759c0f16d0e9d6ee5cee4a3f3711853952c3c
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-09 07:01:20 -0700

    multiplies 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,9 @@
 int main(int argc, char **argv)
 {
 	int i = 1;
-	int mul = i;
+	int mul;
+
+	mul = i;
 
 	if (argc > 1)
 	{
@@ -22,7 +24,7 @@ int main(int argc, char **argv)
 		}
 		printf("%d\n", mul);
 	}
-	if (argc == 1)
+	if (argc == 1 && argc == 2)
 	{
 		printf("Error\n");
 		return (1);
