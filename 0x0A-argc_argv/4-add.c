ef645e07817a93f6c88719d1f7890921c99392da
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-10 06:35:29 -0700

    adds

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,20 +10,28 @@
  */
 int main(int argc, char **argv)
 {
-	int i = 1;
+	int i, j;
 	int sum = 0;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			sum += atoi(argv[i]);
-		}
-		printf("%d\n", sum);
-	}
 	if (argc < 2)
 	{
 		printf("0\n");
+		return (0);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
