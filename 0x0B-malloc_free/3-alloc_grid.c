4c5fd64b02f225171e270762e00b4dbb758979d7
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-11 09:21:47 -0700

    needs fixing

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 {
 	int **ptr1;
 	int i, j;
-	
+
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
@@ -20,6 +20,7 @@ int **alloc_grid(int width, int height)
 	if (*ptr1 == NULL)
 	{
 		return (NULL);
+		free(ptr1);
 	}
 	for (i = 0; i < height; i++)
 	{
