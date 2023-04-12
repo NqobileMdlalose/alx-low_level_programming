dfd9d6ce4112f56efdde41a8788c210246b7aad2
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-12 05:42:46 -0700

    advanced

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -21,7 +21,7 @@ char *argstostr(int ac, char **av)
 	{
 		len += strlen(av[i]) + 1; /* 1 for a new line*/
 	}
-	ptr = malloc(sizeof(av) * len);
+	ptr = malloc(sizeof(char) * len);
 	if (ptr == NULL)
 	{
 		return (NULL);
