b3ab15d7a55cab3e0eeb4fcf585e7ce5fc5fb9a3
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-17 08:46:55 -0700

    struct q1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,6 +10,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	struct dog
+	{
+		char name[20];
+		float age;
+		char owner[20];
+	};
 	int i;
 
 	for (i = 0; name[i] != '\0'; i++)
