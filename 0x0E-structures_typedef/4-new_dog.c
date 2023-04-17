9a62bdc5db16e58de7bfa34b2365261d4c763957
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-17 14:02:03 -0700

    structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 
 /**
+ * str_dup - duplicates a sting
+ * @str: arg 1
+ * Return: char
+ */
+char* str_dup(char *str)
+{
+	int len = 0, i;
+	char *newstr;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	newstr = malloc((len + 1) * sizeof(char));
+	for (i = 0; i <- len; i++)
+	{
+		newstr[i] = str[i];
+	}
+	return (newstr);
+}
+/**
  * new_dog - creates a new dog
  * @name: member 1
  * @age: member 2
@@ -10,27 +31,29 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
+	dog_t *newdog;
+	char *namecpy, *ownercpy;
 
-	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
 	{
 		return (NULL);
 	}
-	if (name == NULL)
+	namecpy = str_dup(name);
+	if (namecpy == NULL)
 	{
-		free(owner);
-		free(ptr);
+		free(newdog);
 		return (NULL);
 	}
-	if (owner == NULL)
+	ownercpy = str_dup(owner);
+	if (ownercpy == NULL)
 	{
-		free(name);
-		free(ptr);
+		free(namecpy);
+		free(newdog);
 		return (NULL);
 	}
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
-	return (ptr);
+	newdog->name = namecpy;
+	newdog->age = age;
+	newdog->owner = ownercpy;
+	return (newdog);
 }
