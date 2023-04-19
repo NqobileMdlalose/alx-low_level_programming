2bfc458cb781a515efee227e07322cdb989d921f
NqobileMdlalose <mdlalose105@gmail.com>
2023-04-19 17:06:15 +0000

    advanced

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	i = atoi(argv[1]);
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	for (j = 0; j < i; j++)
+	{
+		printf("%02hhx ", *((char *)main + j));
+		if (i < j)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+
+
+}
