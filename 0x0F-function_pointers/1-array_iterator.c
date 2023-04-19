6bc9d7e5a8122be9d3e7e9443d47e484365c1f74
NqobileMdlalose <mdlalose105@gmail.com>
2023-04-19 13:50:36 +0000

    function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,18 +3,20 @@
 
 /**
  * array_iterator -  function iterator
- * @array:
- * @size:
- * @action:
+ * @array: pointer
+ * @size: iterator
+ * @action: function pointer
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int j;
-	
+
 	if (action)
-	for (j = 0; j < size; j++ )
 	{
-		action(array[j]);
+		for (j = 0; j < size; j++)
+		{
+			action(array[j]);
+		}
 	}
 }
