a0d5cd3b509f9a9fbf747e9132d20909c4c3dd6f
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-20 04:01:46 -0700

    variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	if (separator == NULL)
 	{
-		return;
+		printf(" ");
 	}
 	for (i = 0; i < n; i++)
 	{
@@ -28,7 +28,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("%d %s", va_arg(ap, const unsigned int), separator);
+			printf("%d%s", va_arg(ap, const unsigned int), separator);
 		}
 	}
 	printf("\n");
