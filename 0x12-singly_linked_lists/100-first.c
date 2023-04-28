f07fb2c348fb9841490f16e3c9d4c12096651916
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-28 04:48:13 -0700

    list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_message - prints the message.
+ * Return: void
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
