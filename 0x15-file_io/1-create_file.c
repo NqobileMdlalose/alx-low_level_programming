6b0d5a5f1a8ed954666815739352cfdcdb14a3a7
NqobileMdlalose <mdlalose150@gmail.com>
2023-05-08 11:57:53 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,20 +16,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, bw = 0, len = 0;
+	int file, fw = 0, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMISSIONS);
-	if (file == -1)
-		return (-1);
-	while (text_content[len] != '\0')
-	{
-		len++;
-	}
 	if (text_content != NULL)
-		bw = write(file, text_content, len);
-	if (bw == -1 || (text_content != NULL && bw != len))
+	{
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+	}
+	fw = write(file, text_content, len);
+	if (fw == -1 || file == -1)
 	{
 		close(file);
 		return (-1);
