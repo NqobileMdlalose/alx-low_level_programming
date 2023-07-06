02602d784f2d4502a02160356e78aaf698b8385d
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-06 08:21:55 -0700

    fixes

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,16 +8,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int bits = 0;
-	unsigned long int temp = n;
 	unsigned int mask = 1UL << index;
 
-	while (temp != 0)
-	{
-		bits++;
-		temp = temp >> 1;
-	}
-	if (index >= bits)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	if (n & mask)
 		return (1);
