cf95288329b6a8ed2c9a4f97125587ab61137a24
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-06 05:14:55 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,4 +7,27 @@
  */
 void print_binary(unsigned long int n)
 {
+	int i, b, bit = 0;
+	unsigned long int temp = n;
+
+	while (temp != 0)
+	{
+		bit++;
+		temp = temp >> 1;
+	}
+	if (bit == 0)
+	{
+		_putchar(48);
+	}
+	else
+	{
+		for (i = bit - 1; i >= 0; i--)
+		{
+			b = n >> i;
+			if (b & 1)
+				_putchar(49);
+			else
+			_putchar(48);
+		}
+	}
 }
