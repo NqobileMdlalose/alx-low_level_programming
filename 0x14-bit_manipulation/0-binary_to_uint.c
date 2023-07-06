4a4995acfb5e73b851480fabe454f135dfeef865
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-06 07:39:31 -0700

    fixes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
 #include <stddef.h>
 /**
  * binary_to_uint - converts binary to decimal
@@ -12,6 +14,10 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	int power = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	while (b[len] != '\0')
 	{
 		len++;
