eb6d254364df3410440a1350eef6e43cae9c3de4
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-06 06:01:33 -0700

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit at a given index
+ * @n: int
+ * @index: index
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bits = 0;
+	unsigned long int temp = n, mask = 1UL << index;
+
+	while (temp != 0)
+	{
+		bits++;
+		temp = temp >> 1;
+	}
+	if (index >= bits)
+		return (-1);
+	if (n & mask)
+		return (1);
+	else
+		return (0);
+
+}
