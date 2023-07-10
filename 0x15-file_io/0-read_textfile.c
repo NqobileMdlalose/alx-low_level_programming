2a7c743ec95ea937d11b95a7f710c239abda7858
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-10 05:30:48 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output 
+ * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file
  * @letters: letters
  * Return: text
