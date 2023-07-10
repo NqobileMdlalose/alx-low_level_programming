f801d962327823eac4b71608ab5f9056456719d9
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-10 06:44:27 -0700

    task 2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
-	buf = malloc((letters + 1) * sizeof(char));
+	buf = malloc((letters) * sizeof(char));
 	if (buf == NULL)
 	{
 		close(file);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,9 +7,34 @@
 /**
  * append_text_to_file -  appends text to the end of an existing file
  * @filename: name of the file
- * @text_content
+ * @text_content: content of the file
  * Return: 0
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int file;
+	ssize_t content_len;
+	ssize_t byte_written;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		content_len = 0;
+		while (text_content[content_len] != '\0')
+		{
+			content_len++;
+		}
+		byte_written = write(file, text_content, content_len);
+		if (byte_written == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
