9cef5f57ab77faed3b53c3e3e7f2b41f9207d816
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-11 05:29:20 -0700

    fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,17 +13,16 @@
  */
 void copy_file(const char *file_from, const char *file_to)
 {
-	int fd_from, fd_to;
+	int fd_from = open(file_from, O_RDONLY);
+	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC);
 	char buf[BUFFER_SIZE];
 	ssize_t byte_read, byte_written;
 
-	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
